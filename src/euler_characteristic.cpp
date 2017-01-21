fd1d37cbea3435d110cf9d9caf4a9f9cb1e7db90
henriquetmaia <henriquetmaia@gmail.com>
2017-01-20 21:40:19 -0500

    torus has 0, bunny has 2 chi

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,23 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
-  int Chi;
-  // ADD YOUR CODE HERE
-  return Chi;
+	int Chi;
+	// Nah man, you add it
+
+	Eigen::VectorXi verts(F.rows() * 3);
+	verts.setZero();
+
+	for( int f = 0; f < F.rows(); ++f ){
+		verts[F(f,0)] = 1;
+		verts[F(f,1)] = 1;
+		verts[F(f,2)] = 1;
+	}
+
+	int V = verts.sum();
+
+	Chi = V - edges(F).rows() + F.rows();
+
+	return Chi;
 }
